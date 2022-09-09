5ea142c0264123a44d39ea9fd685906db4d34550
David Cruwys <david@ideasmen.com.au>
2022-09-09 11:35:37 +1000

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      }
+    },
+    fontFamily: {
+      'poppins': ["'Poppins'", 'sans-serif']
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  plugins: [],
 }
