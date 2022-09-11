98a47b976160e552669b1955a5f4b0bee471285f
David Cruwys <david@ideasmen.com.au>
2022-09-11 19:47:54 +1000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [
-    svelte(),
-    tailwind({
+  integrations: [svelte(), tailwind({
     config: {
       applyBaseStyles: false
     }
-  })]
+  })],
+  adapter: netlify()
 });
