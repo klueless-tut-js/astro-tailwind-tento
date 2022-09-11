c2ccb12e5f7c6c8d2a81eca79f496a6664bbfbaa
David Cruwys <david@ideasmen.com.au>
2022-09-11 15:10:16 +1000

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,19 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "bookmark-purple": "#5267DF",
-        "bookmark-red": "#FA5959",
-        "bookmark-blue": "#242A45",
-        "bookmark-grey": "#9194A2",
-        "bookmark-white": "#f7f7f7",
       }
     },
     fontFamily: {
       'poppins': ["'Poppins'", 'sans-serif']
     },
     container: {
-      center: true,
-      padding: "1rem",
     },
   },
   plugins: [],
